2d2bf2cca94a1f9aab370864dda085a71b4b1d0a
Preksha MV <prekshamv16@gmail.com>
2026-09-16 13:49:58 +0530

    Add Best Time to Buy and Sell Stock solution and documentation

---
+++ arrays-strings/04-best-time-to-buy-and-sell-stock.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+
+    printf("Enter number of days: ");
+    scanf("%d", &n);
+
+    int prices[n];
+
+    printf("Enter the prices: ");
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &prices[i]);
+    }
+
+    int minPrice = prices[0];
+    int maxProfit = 0;
+
+    for (int i = 1; i < n; i++)
+    {
+        if (prices[i] < minPrice)
+        {
+            minPrice = prices[i];
+        }
+
+        if (prices[i] - minPrice > maxProfit)
+        {
+            maxProfit = prices[i] - minPrice;
+        }
+    }
+
+    printf("Maximum profit: %d\n", maxProfit);
+
+    return 0;
+}
